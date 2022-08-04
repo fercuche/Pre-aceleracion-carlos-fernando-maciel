3a6b4e92aa50979b5f55c0ad78a1451f6e9c4dd8
fercuche <fer2.maciel@gmail.com>
2022-08-04 14:20:17 -0300

    creacion de entidades

--- src/main/java/com/alkemy/challenge/entity/Character.java
+++ src/main/java/com/alkemy/challenge/entity/Character.java
@@ -3,8 +3,10 @@ package com.alkemy.challenge.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @NoArgsConstructor
 @Getter
@@ -17,8 +19,33 @@ public class Character {
     @Column(name = "character_id", nullable = false)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "movie_id")
-    private Movie movie;
+    @Column(name = "character_image")
+    private String image;
 
+    @Column(name = "character_name")
+    private String name;
+
+    @Column(name = "character_age")
+    private Integer age;
+
+    @Column(name = "character_weight")
+    private Double weight;
+
+    @Lob
+    @Column(name = "character_story")
+    private String story;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Character character = (Character) o;
+        return id != null && Objects.equals(id, character.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
